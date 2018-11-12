79a17271099ef56108a78723185f203979ff43af
FunnySkillz <sebastijan.bogdan@outlook.com>
2018-11-12 13:32:03 +0100

    UPDATE

--- assignment/chess.cpp
+++ assignment/chess.cpp
@@ -10,3 +10,70 @@
  * Implementation of basic chess functions.
  * ----------------------------------------------------------
  */
+#include "chess.h"
+
+ bool is_piece (struct ChessPiece pc, enum PieceColor color, enum PieceType type)
+ {
+   return pc.color == color && pc.type == type;
+ }
+ void 	init_chess_board (ChessBoard chess_board)
+ {
+   for (int x = 0; x < 8; x++) {
+     for (int y = 0; y < 8; y++) {
+       chess_board[x][y].is_occupied = false;
+     }
+   }
+ }
+ struct ChessSquare * 	get_square (ChessBoard chess_board, File file, Rank rank)
+ {
+   return chess_board[file][rank];
+ }
+ bool 	is_square_occupied (ChessBoard chess_board, File file, Rank rank)
+ {
+   return false;
+ }
+ bool 	add_piece (ChessBoard chess_board, File file, Rank rank, struct ChessPiece piece)
+ {
+   return false;
+ }
+ struct ChessPiece 	get_piece (ChessBoard chess_board, File file, Rank rank)
+ {
+   struct ChessPiece x;
+   return x;
+ }
+ void 	setup_chess_board (ChessBoard chess_board)
+ {
+
+ }
+ bool 	remove_piece (ChessBoard chess_board, File file, Rank rank)
+ {
+   return false;
+ }
+ bool 	squares_share_file (File s1_f, Rank s1_r, File s2_f, Rank s2_r)
+ {
+   return false;
+ }
+ bool 	squares_share_rank (File s1_f, Rank s1_r, File s2_f, Rank s2_r)
+ {
+   return false;
+ }
+ bool 	squares_share_diagonal (File s1_f, Rank s1_r, File s2_f, Rank s2_r)
+ {
+   return false;
+ }
+ bool 	squares_share_knights_move (File s1_f, Rank s1_r, File s2_f, Rank s2_r)
+ {
+   return false;
+ }
+ bool 	squares_share_pawns_move (enum PieceColor color, enum MoveType move, File s1_f, Rank s1_r, File s2_f, Rank s2_r)
+ {
+   return false;
+ }
+ bool 	squares_share_queens_move (File s1_f, Rank s1_r, File s2_f, Rank s2_r)
+ {
+   return false;
+ }
+ bool 	squares_share_kings_move (File s1_f, Rank s1_r, File s2_f, Rank s2_r)
+ {
+   return false;
+ }
